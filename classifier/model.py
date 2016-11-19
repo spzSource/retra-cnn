4209f29f01b504de6cde84115827fe6611ad9fc6
spzSource <sasha.popitich@gmail.com>
2016-11-19 03:26:35 +0300

    Small refactoring

--- classifier/model.py
+++ classifier/model.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from keras.optimizers import SGD
 from keras.models import Sequential
-from keras.layers import Convolution2D, Dense, Flatten, MaxPooling2D, AveragePooling2D
+from keras.layers import Convolution2D, Dense, Flatten, MaxPooling2D, AveragePooling2D, AveragePooling1D
 
 
 class ClassificationModel(object):
@@ -20,29 +20,36 @@ class ClassificationModel(object):
         Builds classification classifier.
         """
         self.model = Sequential([
-            Convolution2D(8, 5, 5, input_shape=(3, 32, 32), activation='relu'),
-            MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'),
-            Convolution2D(16, 3, 3, activation='relu'),
-            MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'),
-            Convolution2D(32, 2, 2, activation='relu'),
-            AveragePooling2D(pool_size=(2, 2), strides=None, border_mode='same'),
-            # MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'),
+            Convolution2D(8, 5, 5, input_shape=(3, 32, 32), activation="relu"),
+            MaxPooling2D(pool_size=(2, 2), strides=None, border_mode="same"),
+            Convolution2D(16, 3, 3, activation="relu"),
+            MaxPooling2D(pool_size=(2, 2), strides=None, border_mode="same"),
+            Convolution2D(32, 2, 2, activation="relu"),
+            AveragePooling2D(pool_size=(2, 2), strides=None, border_mode="same"),
             Flatten(),
-            Dense(10, activation='softmax')
+            Dense(512, activation="relu"),
+            Dense(10, activation="softmax")
         ])
-
         self.model.compile(
-            loss='mse',
-            metrics=['accuracy', 'mean_absolute_percentage_error'],
-            optimizer=SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False))
+            loss="mse",
+            metrics=["accuracy", "mean_absolute_percentage_error"],
+            optimizer=SGD(lr=0.03, momentum=0.0, decay=0.0, nesterov=False))
 
     def train(self, training_set):
-        (inputs, expected_outputs) = training_set
-        self.model.fit(np.array(inputs), np.array(map(self._to_expected_output, expected_outputs)), nb_epoch=1000, batch_size=32)
+        """
+        Performs model training using specified training set,
+        :param training_set: a tuple (x_input, y_expected)
+        :return: None
+        """
 
-    @staticmethod
-    def _to_expected_output(expected_output):
-        class_number = expected_output[0]
-        result = np.zeros(10, dtype=np.int)
-        result[class_number] = 1
-        return result
+        def _to_expected_output(expected_output):
+            class_number = expected_output[0]
+            result = np.zeros(10, dtype=np.int)
+            result[class_number] = 1
+            return result
+
+        (inputs, expected_outputs) = training_set
+        np_input = np.array(inputs)
+        np_expected = np.array(map(_to_expected_output, expected_outputs))
+        print(np_input[0])
+        self.model.fit(np_input, np_expected, batch_size=32, nb_epoch=1000)
